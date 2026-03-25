44459bbdb40b78188643b238f5a06f8e1b82dac4
dcheshirecat <danieldinadin@gmail.com>
2026-03-25 13:03:46 +0200

    Add files via upload

--- tarot.gd
+++ tarot.gd
@@ -1,14 +1,1 @@
-extends Node
-
-var cards = [
-    "The Fool",
-    "The Lovers",
-    "Death",
-    "The Moon"
-]
-
-func draw_card():
-    randomize()
-    var card = cards[randi() % cards.size()]
-    print("Drawn:", card)
-    return card
+class_name Tarot
